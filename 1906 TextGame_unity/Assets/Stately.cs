9749b0d0582c16ee240eab67a5a137832e584b8c
coltonm11 <47471670+coltonm11@users.noreply.github.com>
2019-06-26 00:41:12 -0700

    initial commit

---
+++ 1906 TextGame_unity/Assets/Stately.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Stately")]
+
+public class Stately : ScriptableObject {
+
+    [TextArea(14, 10)] [SerializeField] string storyText;
+    [SerializeField] Stately[] nextStates;
+
+    public Material image;
+
+    public string GetStory()
+    {
+        return storyText;
+    }
+
+    public Stately[] GetNextState()
+    {
+        return nextStates;
+    }
+
+}
---
+++ 1906 TextGame_unity/Assets/TheGame.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TheGame : MonoBehaviour {
+
+    [SerializeField] Text textComponent;
+    public Text textComponentPublicVarForStudy;
+
+    [SerializeField] Stately startingState;
+
+    Stately currentState;
+
+    private void Start()
+    {
+        currentState = startingState;
+        textComponent.text = currentState.GetStory();
+    }
+
+    private void ManageState()
+    {
+        var nextState = currentState.GetNextState();
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            currentState = nextState[0];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            currentState = nextState[1];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            currentState = nextState[2];
+        }
+        textComponent.text = currentState.GetStory();
+    }
+
+}
